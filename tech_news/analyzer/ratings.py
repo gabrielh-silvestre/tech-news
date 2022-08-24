f4a2fcaba1553ec869de42f4e290d1ad33b0f051
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-08-24 12:27:51 -0300

    feat(ratings): implement top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,19 @@
+from typing import Any, Dict, List, Tuple
+
+from tech_news.database import find_news
+
+
 # Requisito 10
-def top_5_news():
-    """Seu código deve vir aqui"""
+def top_5_news() -> List[Tuple[str, int]]:
+    found_news: Dict[str, Any] = find_news()
+
+    # sorted with lambda ref
+    # https://docs.python.org/3/howto/sorting.html#key-functions
+    sorted_news = sorted(
+        found_news, key=lambda news: news["comments_count"], reverse=True
+    )
+
+    return [(news["title"], news["url"]) for news in sorted_news[:5]]
 
 
 # Requisito 11
